a813ebed68b608673678ce7d1f7ce736710bfaf5
aw-jansen <a.w.jansen.1988@gmail.com>
2014-02-28 19:00:43 +0100

    division overview
    Bar that shows percentage of each activity with corresponding colors

--- js/view/dayView.js
+++ js/view/dayView.js
@@ -12,6 +12,22 @@ var DayView = function (container,model,day)
 	var timeCounter = 0;
 	var usableTime;
 
+	var typeDivision = $("<div class='typeDivision'>");
+	var presentationBar = $("<div class='presentationBar'>");
+	var groupworkBar = $("<div class='groupworkBar'>");
+	var discussionBar = $("<div class='discussionBar'>");
+	var brakeBar = $("<div class='brakeBar'>");
+	var warningBrakeBar = $("<div class='warningBrakeBar'>");
+
+	var presentationTime = 0;
+	var groupworkTime = 0;
+	var discussionTime = 0;
+	var brakeTime = 0;
+	var presentationTimePer = 0;
+	var groupworkTimePer = 0;
+	var discussionTimePer = 0;
+	var brakeTimePer = 0;
+
 	timeCounter = parseFloat(day.getStart().slice(0,2)*60);
 	timeCounter += parseFloat(day.getStart().slice(3));
 
@@ -56,7 +72,47 @@ var DayView = function (container,model,day)
 
 		activityBox.append(activityContainer);
 
+		switch(day._activities[j].getType())
+		{
+   			case "Presentation"	:presentationTime += day._activities[j].getLength();break;
+			case "Group Work"	:groupworkTime += day._activities[j].getLength();break;
+			case "Discussion"	:discussionTime += day._activities[j].getLength();break;
+			case "Break"		:brakeTime += day._activities[j].getLength();break;
+		}
 	}
+
+	presentationTimePer = parseInt((presentationTime/day.getTotalLength())*100);
+	groupworkTimePer = parseInt((groupworkTime/day.getTotalLength())*100);
+	discussionTimePer = parseInt((discussionTime/day.getTotalLength())*100);
+	brakeTimePer = parseInt((brakeTime/day.getTotalLength())*100);
+
+	presentationBar.attr("style", "width:"+presentationTimePer+"%");
+	groupworkBar.attr("style", "width:"+groupworkTimePer+"%");
+	discussionBar.attr("style", "width:"+discussionTimePer+"%");
+
+	if(presentationTimePer>0){
+		presentationBar.html(presentationTimePer+"%");	
+	};
+	if(groupworkTimePer>0){
+		groupworkBar.html(groupworkTimePer+"%");
+	};
+	if(discussionTimePer>0){
+		discussionBar.html(discussionTimePer+"%");
+	};
+	if(brakeTimePer>0){
+		brakeBar.html(brakeTimePer+"%");
+	};
+
+			if(brakeTimePer<30)
+		{
+			brakeBar.attr("style", "color:#C40000;width:"+brakeTimePer+"%");
+			warningBrakeBar.html("Too little brake time!");
+		}
+		else
+		{
+			brakeBar.attr("style", "color:#0C7308;width:"+brakeTimePer+"%");
+			warningBrakeBar.html("Enough Brake Time!");
+		}
 
 	/*****************************************  
   			Append items to container  
@@ -64,10 +120,20 @@ var DayView = function (container,model,day)
 	dayTimeBox.append(dayStartBox);
 	dayTimeBox.append(dayEnd);
 
+	typeDivision.append(presentationBar);
+	typeDivision.append(groupworkBar);
+	typeDivision.append(discussionBar);
+	typeDivision.append(brakeBar);
+	typeDivision.append(warningBrakeBar);
+
+
 	container.append(dayTitle);
 	container.append(dayTimeBox);
 	//container.append(dayLength);
 	container.append(activityBox);
+	container.append(typeDivision)
 
     this.activityBox = activityBox;
+
+
 }
